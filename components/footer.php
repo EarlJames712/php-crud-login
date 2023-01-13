734703ea4157f1439978bd6387c1e7b49fee22ca
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-13 15:46:32 +0800

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -1,26 +1,26 @@
 <script>
     $(function () {
         $('#myTable').DataTable();
-        $(".record").submit(function (e){
-            e.preventDefault();
-
-            let firstName = $("#first-name").val();
-            let lastName = $("#last-name").val();
-            let department = $("#department").val();
-
-            $.ajax({
-                type: 'POST',
-                url: 'insertData.php',
-                data: {
-                    fn: firstName,
-                    ln: lastName,
-                    d: department
-                },
-                success: function() {
-                    alert("Form Data Added! Please refresh the page to see results.");
-                }
-            });
-        });
+        // $(".record").submit(function (e){
+        //     e.preventDefault();
+        //
+        //     let firstName = $("#first-name").val();
+        //     let lastName = $("#last-name").val();
+        //     let department = $("#department").val();
+        //
+        //     $.ajax({
+        //         type: 'POST',
+        //         url: 'insertData.php',
+        //         data: {
+        //             fn: firstName,
+        //             ln: lastName,
+        //             d: department
+        //         },
+        //         success: function() {
+        //             alert("Form Data Added! Please refresh the page to see results.");
+        //         }
+        //     });
+        // });
        
     });
 </script>
