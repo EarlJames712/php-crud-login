12e6c96f5bee98f1a9c1f40ee0ecfc1c114b9fb8
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-13 15:46:30 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -5,18 +5,19 @@
     if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
         require_once 'components/header.php';
 ?>
-<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">ACLC Iriga</a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
-  <div class="navbar-nav">
-    <div class="nav-item text-nowrap">
-    <a class="nav-link text-center p-3" href="logout.php"><i class="fa-solid fa-right-from-bracket"></i> Log Out</a>
-    </div>
-  </div>
-</header>
+
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">ACLC Iriga</a>
+      <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
+      <div class="navbar-nav">
+        <div class="nav-item text-nowrap">
+        <a class="nav-link text-center p-3" href="logout.php"><i class="fa-solid fa-right-from-bracket"></i> Log Out</a>
+        </div>
+      </div>
+    </header>
 
 <div class="container-fluid">
   <div class="row">
@@ -104,69 +105,90 @@
           </button>
         </div>
       </div>
+        <div class="mt-3 ">
+            <div class="m-auto">
 
-        <div class="container d-flex justify-content-center mt-5">
-            <center>
-                <ul class="nav nav-tabs justify-content-center mb-4">
-                    <li class="nav-item">
-                        <a class="nav-link text-dark active" aria-current="page" href="#">Main form</a>
-                    </li>
-                </ul>
-                <form class="record" method="post">
-                    <div class="form-floating mb-3">
-                        <input type="text" name="fn" class="form-control" id="first-name" placeholder="Your First Name">
-                        <label for="first-name" class="form-label">First Name</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="text" name="ln" class="form-control" id="last-name" placeholder="Your Last Name">
-                        <label for="last-name" class="form-label">Last Name</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="text" name="d" class="form-control form-control-lg" id="department" placeholder="Department">
-                        <label for="exampleFormControlTextarea1" class="form-label">Department</label>
+                <!-- Add New Button trigger modal -->
+                <button type="button" class="btn btn-success rounded-5 my-4" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    Add New Data
+                </button>
 
+                <!-- Modal -->
+                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Add your data</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <form action="insertData.php" class="record" method="post">
+                            <div class="modal-body">
+                                <ul class="nav nav-tabs justify-content-center mb-4">
+                                    <li class="nav-item">
+                                        <a class="nav-link text-dark active" aria-current="page" href="#">Main form</a>
+                                    </li>
+                                </ul>
+                                <div class="form-floating mb-3">
+                                    <input type="text" name="fn" class="form-control" id="first-name" placeholder="Your First Name" autofocus>
+                                    <label for="first-name" class="form-label">First Name</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="text" name="ln" class="form-control" id="last-name" placeholder="Your Last Name">
+                                    <label for="last-name" class="form-label">Last Name</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="text" name="d" class="form-control form-control-lg" id="department" placeholder="Department">
+                                    <label for="exampleFormControlTextarea1" class="form-label">Department</label>
+
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button class="btn btn-success" type="submit">Add</button>
+                            </div>
+                            </form>
+                        </div>
                     </div>
-                    <button class="btn btn-primary" type="submit">ADD</button>
-                </form>
-                <table id="myTable" class="display table">
+                </div>
+
+                <!--      Data table      -->
+                <table id="myTable" class="display table text-center">
                   <thead>
                   <tr>
-                      <th>#</th>
-                      <th>First Name</th>
-                      <th>Last Name</th>
-                      <th>Department</th>
-                      <th>Delete</th>
-                      <th>Edit</th>
+                      <th class="text-center">#</th>
+                      <th class="text-center">First Name</th>
+                      <th class="text-center">Last Name</th>
+                      <th class="text-center">Department</th>
+                      <th class="text-center">Action/s</th>
                   </tr>
                   </thead>
                   <tbody>
                   <?php
-                  require_once 'conn.php';
-                  $no =  1 ;
-                  $data = mysqli_query($conn, "SELECT * FROM student_list");
-                  while($row = mysqli_fetch_array($data)){
-                      ?>
+                      require_once 'conn.php';
+                      $no =  1 ;
+                      $data = mysqli_query($conn, "SELECT * FROM student_list");
+                      while($row = mysqli_fetch_array($data)){ ?>
                       <tr>
                           <td><?= $no; ?></td>
                           <td><?= $row['firstname']; ?></td>
                           <td><?= $row['lastname']; ?></td>
                           <td><?= $row['department']; ?></td>
-                          <td><a type="button" id="delete" class="btn btn-danger" href="deleteData.php?id=<?= $row['id']?>">DELETE</a></td>
-                          <td><!-- Button trigger modal -->
-                          <a type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editId<?php echo $row['id']?>">
-                            Edit
-                          </a></td>
+                          <td>
+                              <a type="button" id="delete" class="btn btn-danger" href="deleteData.php?id=<?= $row['id']?>">Delete</a>
+                              <a type="button" class="btn btn-primary ms-3" data-bs-toggle="modal" data-bs-target="#editData<?= $row['id']?>">Edit</a>
+                          </td>
+
                       </tr>
                       <!-- Modal -->
-                      <div class="modal fade" id="editId<?php echo $row['id']?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                        <div class="modal-dialog">
+                      <div class="modal fade" id="editData<?= $row['id']?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
                           <div class="modal-content">
                             <div class="modal-header">
-                              <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                              <h1 class="modal-title fs-5" id="staticBackdropLabel">Update <?= $row['firstname']?>'s data</h1>
                               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
-                            <div class="modal-body">
                             <form action="updateData.php" method="post">
+                                <div class="modal-body">
                                   <div class="form-floating mb-3">
                                     <input type="hidden" name="id" value="<?= $row['id']?>">
                                       <input type="text" name="fn" class="form-control" value="<?= $row['firstname']?>" placeholder="Your First Name">
@@ -180,23 +202,19 @@
                                       <input name="d" class="form-control form-control-lg" value="<?= $row['department']; ?>" type="text" placeholder="Department">
                                       <label for="exampleFormControlTextarea1" class="form-label">New Department</label>
                                   </div>
-                                  <button class="btn btn-success" type="submit">UPDATE</button>
-                                  <a href="javascript:history.back()" class="btn btn-primary">Go Back</a>
-                              </form>
-                            </div>
-                            <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                              <button type="button" class="btn btn-primary">Understood</button>
-                            </div>
+                                </div>
+                                <div class="modal-footer">
+                                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button class="btn btn-success" type="submit">Update</button>
+                                </div>
+                            </form>
                           </div>
                         </div>
                       </div>
                       <?php $no++; } ?>
                   </tbody>
                 </table>
-              
-
-            </center>
+            </div>
         </div>
     </main>
   </div>
