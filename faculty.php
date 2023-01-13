df96471510e78ee4681a31c86b1efb9042cf4e96
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-13 15:46:34 +0800

    Update faculty.php

--- faculty.php
+++ faculty.php
@@ -104,38 +104,61 @@
           </button>
         </div>
       </div>
-        <div class="container d-flex justify-content-center mt-5">
-            <center>
-                <ul class="nav nav-tabs justify-content-center mb-4">
-                    <li class="nav-item">
-                        <a class="nav-link text-dark active" aria-current="page" href="#">Main form</a>
-                    </li>
-                </ul>
-                <form class="record" method="post">
-                    <div class="form-floating mb-3">
-                        <input type="text" name="fn" class="form-control" id="first-name" placeholder="Your First Name">
-                        <label for="first-name" class="form-label">First Name</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="text" name="ln" class="form-control" id="last-name" placeholder="Your Last Name">
-                        <label for="last-name" class="form-label">Last Name</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="text" name="d" class="form-control form-control-lg" id="department" placeholder="Department">
-                        <label for="exampleFormControlTextarea1" class="form-label">Department</label>
+        <div class="mt-3 ">
+            <div class="m-auto">
 
-                    </div>
-                    <button class="btn btn-primary" type="submit">ADD</button>
-                </form>
+                <!-- Add New Button trigger modal -->
+                <button type="button" class="btn btn-success rounded-5 my-4" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    Add New Data
+                </button>
 
-                <table class="table">
+                <!-- Modal -->
+                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Add your data</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <form action="insertData.php" class="record" method="post">
+                                <div class="modal-body">
+                                    <ul class="nav nav-tabs justify-content-center mb-4">
+                                        <li class="nav-item">
+                                            <a class="nav-link text-dark active" aria-current="page" href="#">Main form</a>
+                                        </li>
+                                    </ul>
+                                    <div class="form-floating mb-3">
+                                        <input type="text" name="fn" class="form-control" id="first-name" placeholder="Your First Name" autofocus>
+                                        <label for="first-name" class="form-label">First Name</label>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input type="text" name="ln" class="form-control" id="last-name" placeholder="Your Last Name">
+                                        <label for="last-name" class="form-label">Last Name</label>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input type="text" name="d" class="form-control form-control-lg" id="department" placeholder="Department">
+                                        <label for="exampleFormControlTextarea1" class="form-label">Department</label>
+
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button class="btn btn-success" type="submit">Add</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <!--      Data table      -->
+                <table id="myTable" class="display table text-center">
                     <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Department</th>
-                        <th scope="col" colspan="2" class="text-center">Action</th>
+                        <th class="text-center">#</th>
+                        <th class="text-center">First Name</th>
+                        <th class="text-center">Last Name</th>
+                        <th class="text-center">Department</th>
+                        <th class="text-center">Action/s</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -143,25 +166,60 @@
                     require_once 'conn.php';
                     $no =  1 ;
                     $data = mysqli_query($conn, "SELECT * FROM student_list");
-                    while($row = mysqli_fetch_array($data)){
-                        ?>
+                    while($row = mysqli_fetch_array($data)){ ?>
                         <tr>
                             <td><?= $no; ?></td>
                             <td><?= $row['firstname']; ?></td>
                             <td><?= $row['lastname']; ?></td>
                             <td><?= $row['department']; ?></td>
-                            <td><a type="button" id="delete" class="btn btn-danger" href="deleteData.php?id=<?= $row['id']?>">DELETE</a></td>
-                            <td><a type="button" id="edit" class="btn btn-warning" href="editData.php?id=<?= $row['id']?>">EDIT</a></td>
+                            <td>
+                                <a type="button" id="delete" class="btn btn-danger" href="deleteData.php?id=<?= $row['id']?>">Delete</a>
+                                <a type="button" class="btn btn-primary ms-3" data-bs-toggle="modal" data-bs-target="#editData<?= $row['id']?>">Edit</a>
+                            </td>
+
                         </tr>
+                        <!-- Modal -->
+                        <div class="modal fade" id="editData<?= $row['id']?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Update <?= $row['firstname']?>'s data</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <form action="updateData.php" method="post">
+                                        <div class="modal-body">
+                                            <div class="form-floating mb-3">
+                                                <input type="hidden" name="id" value="<?= $row['id']?>">
+                                                <input type="text" name="fn" class="form-control" value="<?= $row['firstname']?>" placeholder="Your First Name">
+                                                <label for="first-name" class="form-label">New First Name</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="ln" class="form-control" value="<?= $row['lastname']; ?>" id="floatingPassword" placeholder="Your Last Name">
+                                                <label for="floatingPassword">New Last Name</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input name="d" class="form-control form-control-lg" value="<?= $row['department']; ?>" type="text" placeholder="Department">
+                                                <label for="exampleFormControlTextarea1" class="form-label">New Department</label>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <button class="btn btn-success" type="submit">Update</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                         <?php $no++; } ?>
                     </tbody>
                 </table>
-
-            </center>
+            </div>
         </div>
     </main>
   </div>
 </div>
+
+
         <?php
         require_once 'components/footer.php';
     }
